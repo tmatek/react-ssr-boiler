30ec52b65e0d1b500234775226d818a6edcd65ef
Tadej Matek <matektadej@gmail.com>
2021-02-05 12:29:33 +0100

    add code-splitting comment.

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ import IndexPage from './pages'
 import FetchPage from './pages/fetch'
 import NotFoundPage from './pages/404'
 
+/* As an optimization, look into code-splitting pages: https://reach.tech/router/large-scale */
 const App = ({ serverData }) => (
   <Router>
     <IndexPage path="/" serverData={serverData} />
